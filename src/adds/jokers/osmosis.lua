05a6d983375a69158b150af515bda849e6ccecb5
Mills-44 <mylesbrodsky44@gmail.com>
2025-08-16 09:01:56 -0500

    Add files via upload

--- src/adds/jokers/osmosis.lua
+++ src/adds/jokers/osmosis.lua
@@ -1,31 +1,33 @@
 SMODS.Joker {
-    atlas = "Joker",
     key = "osmosis",
-    pos = {
-        x = 3,
-        y = 3
-    },
     rarity = 2,
     cost = 6,
+    atlas = "Joker",
+    pos = { 
+        x = 0, 
+        y = 0 
+    },
+    config = { 
+        extra = { 
+            mult = 5 
+        } 
+    },
     blueprint_compat = true,
     eternal_compat = true,
     perishable_compat = true,
-    config = {
-        extra = {
-            mult = 5,
-            minus = 2
-        }
-    },
     loc_vars = function(self, info_queue, card)
-        local size = G.GAME.starting_deck_size - card.ability.extra.minus
-        return { vars = { card.ability.extra.mult, size, card.ability.extra.mult * math.max(0, G.playing_cards and #G.playing_cards - size or 0) } }
+        return { 
+            vars = { 
+                card.ability.extra.mult, 
+                card.ability.extra.mult * (#G.playing_cards - 50)
+            }  
+        }
     end,
     calculate = function(self, card, context)
-        if context.joker_main and not context.individual then
-            local size = G.GAME.starting_deck_size - card.ability.extra.minus
-            if #G.playing_cards > size then
-                return { mult = card.ability.extra.mult * (#G.playing_cards - size) }
-            end
+        if context.joker_main then
+            return {
+                mult = card.ability.extra.mult * (#G.playing_cards - 50)
+            }
         end
     end
 }

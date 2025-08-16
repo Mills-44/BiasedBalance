ee95c7424a7103da3aea6b954859781c5656a14e
Mills-44 <mylesbrodsky44@gmail.com>
2025-08-16 15:32:12 -0500

    Add files via upload

---
+++ src/adds/backs/hibiscus.lua
@@ -0,0 +1,13 @@
+SMODS.Back {
+    key = 'Hibiscus',
+    atlas = 'Backs',
+    pos = {
+        x = 3,
+        y = 0
+    },
+    config = {
+    },
+    calculate = function(self, card, context) 
+    
+    end
+}
---
+++ src/adds/backs/legendary.lua
@@ -0,0 +1,13 @@
+SMODS.Back {
+    key = 'Legendary',
+    atlas = 'Backs',
+    pos = {
+        x = 3,
+        y = 0
+    },
+    config = {
+    },
+    calculate = function(self, card, context) 
+    
+    end
+}
